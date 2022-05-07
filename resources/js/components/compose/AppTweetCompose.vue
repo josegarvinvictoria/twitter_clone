852f0355fb58bfa36460be70b1997bf6769b7492
josegarvinvictoria <josegarvinvictoria@gmail.com>
2022-05-07 21:07:04 +0200

    Setting up realtime (WIP)

--- resources/js/components/compose/AppTweetCompose.vue
+++ resources/js/components/compose/AppTweetCompose.vue
@@ -4,7 +4,6 @@
             <img :src="$user.avatar" class="w-12 rounded-full">
         </div>
         <div class="flex-grow">
-            {{ form }}
             <textarea
                 class="bg-gray-900 w-full outline-none text-gray-300 text-lg resize-none mb-2"
                 placeholder="What's happening?"
